c61c6a616d39c0cad7e435746423ea94c9f8c4f9
kinderror <87951557+kinderror@users.noreply.github.com>
2023-05-19 07:56:50 +0300

    Update a2.m

--- a2.m
+++ a2.m
@@ -1,3 +1,4 @@
+% создаем триангулированную сетку и рисуем
 rng default;
 P = rand(12,2);
 T = delaunayTriangulation(P);
@@ -10,25 +11,25 @@ yHull = T.Points(k,2);
 
 plot(xHull,yHull,'r','LineWidth',2); 
 hold off
+
 bounder = [xHull, yHull];
 bounder
 [sz,numCols] = size(bounder);
 
+% находим внешнюю границу и выводим 
 Min = min(bounder(:,1));
-k=0;
-
-
+k = 0;
 for i = 1:sz 
     if bounder(i,1) == Min
         k = i;
     end
 end
 for j = k:sz 
-    bounder1(j-k+1,:) = bounder(j,:); 
+    out_bounder(j-k+1,:) = bounder(j,:); 
 end
 for j = 1:k-1 
-    bounder1(sz-k+j,:) = bounder(j,:) ;
+    out_bounder(sz-k+j,:) = bounder(j,:) ;
 end
 
-bounder1
+out_bounder
 
