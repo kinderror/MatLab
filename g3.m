1d4698cc9680858dda408fbae57e065d10650c57
kinderror <87951557+kinderror@users.noreply.github.com>
2023-05-19 08:41:20 +0300

    Update g3.m

--- g3.m
+++ g3.m
@@ -1,15 +1,15 @@
 function M = g3(TR)
 [F,P] = freeBoundary(TR);
-n = size(P);
-n = n(1);
-M = zeros(n);
+n = size(P, 1);
+M = zeros(n); % матрица обратных расстояний
 for k = 1 : n
-    for l = k + 1 : n
-        M(k, l) = 1/define_distance(P(k, :), P(l, :));
-        M(l, k) = M(k, l);
+    for m = k + 1 : n
+        M(k, m) = 1/calculate_distance(P(k, :), P(m, :));
+        M(m, k) = M(k, m); % из-за симметрии
     end
 end
 
-function s = define_distance(v1, v2)
+% функция вычисления расстояния
+function s = calculate_distance(v1, v2)
 s = sqrt(sum((v1 - v2).^2));
  
