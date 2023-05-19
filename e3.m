94a95c7df07344d6375f6a31d4e83f6e2f6d06d9
kinderror <87951557+kinderror@users.noreply.github.com>
2023-05-19 08:27:51 +0300

    Update e3.m

--- e3.m
+++ e3.m
@@ -1,26 +1,22 @@
 function area = e3(TR)
-    % TR is a triangulation object in 3D
-
-    % Get the indices of the vertices in each triangle
+    % TR - объемная триангулированная сетка
+    % Находим индексы всех вершин
     triangles = TR.ConnectivityList;
 
-    % Compute the areas of each triangle
+    % Вычислим площади каждого треугольника
     vec1 = TR.Points(triangles(:, 2), :) - TR.Points(triangles(:, 1), :);
     vec2 = TR.Points(triangles(:, 3), :) - TR.Points(triangles(:, 1), :);
     cross_prod = cross(vec1, vec2, 2);
     triangle_areas = 0.5 * sqrt(sum(cross_prod .^ 2, 2));
 
-    % Initialize the area array to zeros
+    % Создаем матрицу площадей
     n = size(TR.Points, 1);
     area = zeros(n, 1);
 
-    % Loop over each triangle
     for k = 1:size(triangles, 1)
-        % For each vertex in this triangle, add the area of the triangle
-        % divided by 3 to its corresponding entry in the area array
+        % К каждой вершине треугольника добавляем его площадь и делим на 3
         for j = 1:3
             area(triangles(k, j)) = area(triangles(k, j)) + triangle_areas(k) / 3;
         end
-
     end
 end
