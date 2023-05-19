a1b077412ae3ff4dc1c2942497843cde612f3bb9
kinderror <87951557+kinderror@users.noreply.github.com>
2023-05-19 08:10:47 +0300

    Update c2.m

--- c2.m
+++ c2.m
@@ -1,19 +1,18 @@
-function t3(a,b,c)
+function c2(a,b,c)
     arguments
         a = [1 1 1]; 
         b = [-1 1 -1];
         c = [-1 -1 1];
     end
 
-    % define the size of the grid as n
     n = 2;
     grid_points = [];
-    % create a 3D grid of size 
+    % создаем 3D сетку размера n
     index = 1;
     for i = -1:1
         for j = -1:1
             for k = -1:1
-                grid_points(end+1,:) = a*i + j*b+k*c;
+                grid_points(end+1,:) = a * i + j*b+k*c;
                 if (i ~= 0) || (j ~= 0) || (k ~= 0)
                     index = index + 1;
                 else
@@ -23,24 +22,26 @@ function t3(a,b,c)
         end 
     end
 
-
+    % воспользуемся диаграммами Вороного
     [v, ce] = voronoin(grid_points);
 
     vertices = v(ce{cell_idx},:) - repmat([0,0,0],size(v(ce{cell_idx},:),1),1);
-
+    
+    % создаем триаунгулированную сетку по точкам
     tri = delaunay(vertices(:,1), vertices(:,2), vertices(:,3));
-
 
     hold on;
     scatter3(vertices(:,1), vertices(:,2), vertices(:,3), 'b')
     scatter3(grid_points(:,1), grid_points(:,2), grid_points(:,3), 'r')
-
+    
+    % рисуем триангулированную сетку
     j = boundary(grid_points,1);
     trisurf(j,grid_points(:,1), grid_points(:,2), grid_points(:,3),'FaceColor','red','FaceAlpha',0.1)
 
     j = boundary(vertices,1);
     trisurf(j,vertices(:,1), vertices(:,2), vertices(:,3),'FaceColor','blue','FaceAlpha',0.1)
     
+    % сжимаем векторы
     a = a * 0.5 / norm(a);
     b = b * 0.5 / norm(b);
     c = c * 0.5 / norm(c);
@@ -53,8 +54,5 @@ function t3(a,b,c)
     ylabel('Y')
     zlabel('Z')
     axis equal
-
-
-
 
 end
