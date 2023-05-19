dd710a89e829c4612ed84a8936c49d7809304f46
kinderror <87951557+kinderror@users.noreply.github.com>
2023-05-19 08:47:14 +0300

    Update cosmo_matrix.m

--- cosmo_matrix.m
+++ cosmo_matrix.m
@@ -2,19 +2,20 @@ function M = cosmo_matrix(P,S)
 % P - матрица nx3 с координатами вершин триангулированной сетки. n - число вершин
 % S - вектор nx1 со значениями площадей поверхностных элементов.
 % M - матрица nxn определяющая потенциал в вершинах сетки по значениям зарядов в них.
-n = size(P);
-n = n(1);
+n = size(P, 1);
 M = zeros(n, n);
-for k = 1:n
-    for l = 1:n
-        if k == l 
-            M(k, l) = 2*sqrt(pi*S(l));
+for i = 1:n
+    for j = 1:n
+        if i == j 
+            M(i, j) = 2*sqrt(pi*S(i));
         else
-            M(k, l) = S(l)/dist(P(k, :), P(l, :));
+            M(i, j) = S(j)/calculate_distance(P(i, :), P(j, :));
         end
     end
 end
+end
 
-
-function r = dist(v1, v2)
-r = sqrt(sum((v1 - v2).^2));
+% функция вычисления расстояния
+function L = calculate_distance(v1, v2)
+L = sqrt(sum((v1 - v2).^2));
+end
