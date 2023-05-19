f35a42f377b50ee1e155ca1404db29039b555e49
kinderror <87951557+kinderror@users.noreply.github.com>
2023-05-19 08:16:14 +0300

    Update c3.m

--- c3.m
+++ c3.m
@@ -2,14 +2,15 @@ function Dy = c3(TR)
     arguments
         TR = delaunayTriangulation(rand(12,2));
     end
+    % стороны и вершины триангулированной сетки
     Edges = TR.edges;
     Points = TR.Points;
     n = size(Points, 1);
-
+    
+    % матрица производной по y
     Dy = sparse(n, n);
 
     for k = 1:length(Edges)
-
         i = Edges(k, 1);
         j = Edges(k, 2);
 
@@ -21,8 +22,5 @@ function Dy = c3(TR)
             Dy(j, i) = Dy(j, i) + 1 / delta_y;
             Dy(j, j) = Dy(j, j) - 1 / delta_y;
         end
-
     end
-
-
 end
